78affc3420f4ac415a054e84a36c15da0c38009d
son.geon <son.geon@jtbc.co.kr>
2026-01-21 14:03:07 +0900

    .env 파일 세팅

--- src/main/java/com/auto/qa/AgentApplication.java
+++ src/main/java/com/auto/qa/AgentApplication.java
@@ -1,11 +1,13 @@
 package com.auto.qa;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgentApplication {
     public static void main(String[] args) {
+        Dotenv.load();
         SpringApplication.run(AgentApplication.class, args);
     }
 }
